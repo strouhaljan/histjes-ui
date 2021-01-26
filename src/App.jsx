9e81f4143bee19a8d1878fef54f3d4cb961b65a2
Martin Vlk <mavlk@hfbiz.cz>
2021-01-26 21:01:23 +0100

    extract theme to separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { createTheme, loadTheme } from "@fluentui/react";
 import { ThemeProvider } from "@fluentui/react-theme-provider";
 
 import { initializeIcons } from "@fluentui/react/lib/Icons";
@@ -9,34 +8,8 @@ import "./App.scss";
 import { Map } from "./components/map/Map";
 import { SidePanel } from "./components/sidePanel/SidePanel";
 
-const theme = createTheme({
-  palette: {
-    themePrimary: "#2e2e78",
-    themeLighterAlt: "#f3f3fa",
-    themeLighter: "#d2d2e9",
-    themeLight: "#afafd6",
-    themeTertiary: "#6d6dae",
-    themeSecondary: "#3e3e88",
-    themeDarkAlt: "#29296c",
-    themeDark: "#23235b",
-    themeDarker: "#1a1a43",
-    neutralLighterAlt: "#faf9f8",
-    neutralLighter: "#f3f2f1",
-    neutralLight: "#edebe9",
-    neutralQuaternaryAlt: "#e1dfdd",
-    neutralQuaternary: "#d0d0d0",
-    neutralTertiaryAlt: "#c8c6c4",
-    neutralTertiary: "#a19f9d",
-    neutralSecondary: "#605e5c",
-    neutralPrimaryAlt: "#3b3a39",
-    neutralPrimary: "#323130",
-    neutralDark: "#201f1e",
-    black: "#000000",
-    white: "#ffffff",
-  },
-});
+import { theme } from "./theme";
 
-loadTheme(theme);
 initializeIcons();
 
 const defaultLayersState = [
